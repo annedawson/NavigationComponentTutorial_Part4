914ac9f0f792f5785db35d44a62d7760c691a0db
annedawson <anne.dawson@gmail.com>
2021-03-22 10:42:09 -0700

    Commit 22/3/2021 10:41 PT for new comments explaining use of action code

--- app/src/main/java/com/codinginflow/navigationcomponenttutorial/LoginFragment.kt
+++ app/src/main/java/com/codinginflow/navigationcomponenttutorial/LoginFragment.kt
@@ -10,7 +10,14 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+//  IMPORTANT, you have to Build-> Rebuild project after typing in the following line
+        //  (before typing in val action
+        //  See the tutorial video Part 3 https://youtu.be/5OpE7ftsYG8 at 5m 40s
+        // something to do with the use of safeargs plugin which needs to
+        // generate some code that only happens at compile time.
+        // If Build->Rebuild is successful the HomeFragmentDirections class below
+        // should be automatically generated with the name taken from the
+        // HomeFragment class. It should show up automatically as you type it in.
         button_confirm.setOnClickListener {
             val username = edit_text_username.text.toString()
             val password = edit_text_password.text.toString()
--- app/src/main/java/com/codinginflow/navigationcomponenttutorial/WelcomeFragment.kt
+++ app/src/main/java/com/codinginflow/navigationcomponenttutorial/WelcomeFragment.kt
@@ -16,7 +16,14 @@ class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
 
         text_view_username.text = args.username
         text_view_password.text = args.password
-
+//  IMPORTANT, you have to Build-> Rebuild project after typing in the following line
+        //  (before typing in val action
+        //  See the tutorial video Part 3 https://youtu.be/5OpE7ftsYG8 at 5m 40s
+        // something to do with the use of safeargs plugin which needs to
+        // generate some code that only happens at compile time.
+        // If Build->Rebuild is successful the HomeFragmentDirections class below
+        // should be automatically generated with the name taken from the
+        // HomeFragment class. It should show up automatically as you type it in.
         button_ok.setOnClickListener {
             val action = WelcomeFragmentDirections.actionWelcomeFragmentToHomeFragment()
             findNavController().navigate(action)
